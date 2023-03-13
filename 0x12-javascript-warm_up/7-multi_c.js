b48da11acd4a866bd68d424acb5697c3214670d4
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-13 23:57:27 +0000

    7-multi_c.js updated

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const statement = 'C is fun';
-const x = process.argv[2];
-const numOfTimes = parseInt(x);
+const numOfTimes = process.argv[2];
+const x = parseInt(numOfTimes);
 
 if (isNaN(numOfTimes)) {
 		console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < numOfTimes; i++) {
+	for (let i = 0; i < x; i++) {
 		console.log(statement);
 	}
 }
