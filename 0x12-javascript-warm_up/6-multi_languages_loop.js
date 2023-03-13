b9a7f59bc449e017776c649422a57aa0cf9de1bf
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-13 22:59:43 +0000

    6-multi_languages_loop.js updated

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const statements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < statements.length; i++) {
+  console.log(statements[i]);
+}
+/*
+let i = 0;
+while (i < statements.length) {
+  console.log(statements[i]);
+  i++;
+}
+let i = 0;
+do {
+  console.log(statements[i]);
+  i++;
+} while (i < statements.length);
+*/
