2a35c6b86196b2582ec4b899b32fac1407550abb
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-13 17:11:13 +0000

    2-arguments.js updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const argument = process.argv.slice(2);
-
 if (argument.length === 0) {
-  console.log('No argument');
+	console.log('No argument');
 } else if (argument.length === 1) {
-  console.log('Argument found');
+	console.log('Argument found');
 } else {
-  console.log('Argument found');
+	console.log('Argument found');
 }
