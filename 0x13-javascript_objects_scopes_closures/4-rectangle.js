5d00bfaa2c52f30ab09a5d8d84533bfde49d8d38
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-14 17:45:43 +0000

    4-rectangle.js pushed

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+	constructor(w, h) {
+		if (w<=0 || h<=0 || isNaN(w) || isNaN(h)) {
+			return {}
+		}
+		this.width = w;
+		this.height = h;
+	}
+	print() {
+		for (let i=0; i<this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
+	rotate() {
+		for (let i=0; i<this.width; i++) {
+			console.log('X'.repeat(this.height));
+		}
+	}
+	double() {
+		const doubleWidth = this.width * 2;
+		const doubleHeight = this.height * 2;
+		for (let i=0; i<doubleHeight; i++) {
+			console.log('X'.repeat(doubleWidth));
+		}
+	}
+	
+}
+module.exports = Rectangle;
