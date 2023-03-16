a7051c706a834c1038820762cc6f17797dab7e6c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-16 18:14:43 +0000

    1-rectangle.js updated

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,12 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {};
+      return 'Rectangle {}';
     } else {
       this.width = w;
       this.height = h;
+
+      return `Rectangle { width: ${this.width}, height: ${this.height} }`;
     }
   }
 }
