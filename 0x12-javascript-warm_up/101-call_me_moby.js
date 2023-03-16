e8ac6c965448e10e4b40ea3b67b85d41216ac63e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-16 13:16:01 +0000

    101-call_me_moby.js updated

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,23 @@
 #!/usr/bin/node
 function callMeMoby (x, theFunction) {
+  if (typeof x !== 'number') {
+    x = parseInt(x);
+  }
+  if (typeof theFunction !== 'function') {
+    theFunction = function () {};
+  }
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 }
+
+// Or
+
+/**
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+**/
 module.exports.callMeMoby = callMeMoby;
