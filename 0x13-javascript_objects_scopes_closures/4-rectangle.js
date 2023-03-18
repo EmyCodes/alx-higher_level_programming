678d197c83f4907b454a9bf59907c16de1c708d8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-18 05:51:20 +0000

    4-rectangle.js updated

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -21,8 +21,8 @@ module.exports = class Rectangle {
 	 double () {
 		 this.width *= 2;
 		 this.height *= 2;
-		 for (let i = 0; i < this.height; i++) {
+/*		 for (let i = 0; i < this.height; i++) {
 			 console.log('X'.repeat(this.width));
-		 }
+		 }*/
 	 }
 };
