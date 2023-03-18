247b462d9e50d2c79786fd2b8d1cfc82160fbb02
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-18 05:39:17 +0000

    4-rectangle.js updated

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,35 +1,27 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
-  }
+module.exports = class Rectangle {
+	 constructor (w, h) {
+		 if (w > 0 && h > 0) {
+			 this.width = w;
+			 this.height = h;
+		 }
+	 }
 
-  print () {
-    if (!this.width || !this.height) {
-      // Do nothing if width or height is not defined
-      return;
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
-    }
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-}
-module.exports = Rectangle;
+	 print () {
+		 for (let i = 0; i < this.height; i++) {
+			 console.log('X'.repeat(this.width));
+		 }
+	 }
+	 rotate () {
+		 for (let i = 0; i < this.width; i++) {
+			 console.log('X'.repeat(this.height));
+		 }
+	 }
+	 double () {
+		 this.width *= 2;
+		 this.height *= 2;
+		 for (let i = 0; i < this.height; i++) {
+			 console.log('X'.repeat(this.width));
+		 }
+	 }
+};
