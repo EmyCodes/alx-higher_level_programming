99c673afe86ff30a585aa1c96b939875915a0dba
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-19 23:54:37 +0000

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+	username = sys.argv[1]
+	password = sys.argv[2]
+	database = sys.argv[3]
+	
+	conn = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+	# Assign cursor
+	cur = conn.cursor()
+	sql = "SELECT * FROM states ORDER BY id ASC"
+	cur.execute(sql)
+	# Get items
+	states = cur.fetchall()
+	for state in states:
+		print(state)
+	cur.close()
+	conn.close()
