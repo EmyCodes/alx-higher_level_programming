d62364ec007233404840b5b13e993527808bd91c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-20 10:15:12 +0000

    4-cities_by_state.py pushed

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities from the database
+"""
+
+import MySQLdb
+import sys
+
+"""
+code should not be executed when imported
+"""
+
+if __name__ == "__main__":
+	# Open Connection
+	connection = MySQLdb.connect(host='localhost',port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	# close connection
+	cursor = connection.cursor()
+	sql = "SELECT * FROM cities ORDER BY id ASC"
+	cursor.execute(sql)
+	cities = cursor.fetchall()
+
+	# Loop through
+	for city in cities:
+		print(city)
+	
+	# Close Cursor
+	cursor.close()
+	# close connection
+	connection.close()
