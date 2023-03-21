25eb5fb5a59d5cff956a9b8da6b7df9af284a34b
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-21 23:55:21 +0000

    7-model_state_fetch_all.py updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,14 +16,14 @@ if makses the files in-executble when imported.
 if __name__ == "__main__":
 	engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-	#Create Tables
+	#Create all Tables associated with the Base metadata
 	Base.metadata.create_all(engine)
 
-	# Creating Session
+	# Creating a new Session instance bound to the engine we created
 	Session = sessionmaker(bind=engine)
 	session = Session()
 
-	# Interacting with the database
+	# Interacting with the database by query all state objects from the database and order by states.id
 
 	states_list = session.query(State).order_by(State.id).all()
 
