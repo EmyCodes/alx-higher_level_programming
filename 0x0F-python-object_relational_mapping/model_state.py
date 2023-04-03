c1ef83d65b168d858d3aaac0d4f7bedd5b3e0266
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-03 14:30:07 +0000

    model files

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Write a python file that contains the class definition of a State and an instance Base = declarative_base()
+""" Defines a State class
+    Inherits from SQLAlchemy Base and links to the MySQL table states
 """
 
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, Column, String, create_engine
 
-
-# create engine
 Base = declarative_base()
 
+
 class State(Base):
-	"""
-	Class inherit Base Model
+    """ Represents a state column for a MySQL table
 
-	__tablename__ (str): Table name.
-	id (sqlalchemy.Integer): state's id.
-	name (sqlalchemy.String): state name.
-
-	"""
-	__tablename__ = "states"
-	id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-	name = Column(String(128), nullable=False)
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
