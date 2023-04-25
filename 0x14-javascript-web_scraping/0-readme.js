7f030b3c56306f6b3ee6867f8ac7be1ff0746a70
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-25 18:14:21 +0000

    gave the function a new style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 // Read the file and print its content
-fs.readFile(filePath, 'utf-8', (error, fileContent) => {
+fs.readFile(filePath, 'utf-8', function (error, fileContent) {
   if (error) {
     console.error(error);
   } else {
