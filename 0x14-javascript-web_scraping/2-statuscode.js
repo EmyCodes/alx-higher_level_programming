09d00ccf3cd148400b2f28a92d2413137b47bd20
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-25 19:27:38 +0000

    foramttedtoo pass correct output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,8 +9,8 @@ const url = process.argv[2];
 // Querying the url
 request.get(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
