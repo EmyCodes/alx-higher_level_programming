fa5e8226fdb91d34ccad283c8525ece70134a74e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-25 18:10:34 +0000

    semicolons added to line 3 and 6 (semistandard)

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Import fs module
-const fs = require('fs')
+const fs = require('fs');
 
 // Assign file path
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 
 // Read the file and print its content
 fs.readFile(filePath, 'utf-8', (error, fileContent) => {
