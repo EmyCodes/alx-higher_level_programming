3cc8f6aa7b783b4ac2049226f0bf189035754687
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-04-25 19:24:58 +0000

    fix to pass semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,13 +4,13 @@
 
 // import request and declare needed variables
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 
 // Querying the url
-request.get(url, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
+request.get(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     console.log('code: ', response.statusCode);
   } else {
     console.log('code: ', response.statusCode);
-    }
+  }
 });
